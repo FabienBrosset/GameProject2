c0334780e39ebe3cea139d456d4923adbfd32207
Nicolas Goepp <nicolas.goepp@epitech.eu>
2021-05-30 18:08:08 +0900

    Scene TransitionIn

--- Assets/Scripts/PlayerLifeManager.cs
+++ Assets/Scripts/PlayerLifeManager.cs
@@ -6,13 +6,31 @@ using UnityEngine.SceneManagement;
 public class PlayerLifeManager : MonoBehaviour
 {
     public int hp = 5;
+    public GameObject transitionPrefab;
 
     private void Update()
     {
         if (hp <= 0)
         {
-            PlayerPrefs.SetInt("player_score", -1);
-            SceneManager.LoadScene(5);
+
+            GameObject.Find("MusicPlayer").transform.GetComponent<AudioSource>().pitch = 0.75f;
+
+            GameObject _trans = Instantiate(transitionPrefab);
+
+            _trans.transform.parent = GameObject.Find("Canvas").transform;
+
+            _trans.transform.localPosition = new Vector3(0f, 0f, 0f);
+            _trans.transform.localScale = new Vector3(1f, 1f, 1f);
+
+            StartCoroutine(WaitFor());
         }
+    }
+
+    IEnumerator WaitFor()
+    {
+        yield return new WaitForSeconds(3);
+
+        PlayerPrefs.SetInt("player_score", -1);
+        SceneManager.LoadScene(5);
     }
 }
